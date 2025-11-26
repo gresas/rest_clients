ae88b1294b10ca1a1718c7b1f4d8885876be1576
gresas <gbrondon@id.uff.br>
2025-11-25 22:44:47 -0300

    Deleting old files

--- exceptions.py
+++
@@ -1,8 +0,0 @@
-class ApiRestException(Exception):
-    def __init__(self, *args, **kwargs):
-        self.status = kwargs.get('status')
-        super().__init__(*args)
-
-
-class MissingConfigurationException(ValueError):
-    pass
